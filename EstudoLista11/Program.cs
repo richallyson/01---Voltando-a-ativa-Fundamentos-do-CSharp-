96b8fc18cf87e67bfc898ffd986a598eae7e0811
Richallyson Lima <richallyson@gmail.com>
2022-04-21 19:04:06 -0300

    Adicionando mais práticas com array/matriz

--- EstudoLista11/Program.cs
+++ EstudoLista11/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Threading;
 
 namespace EstudoLista11
 {
@@ -7,6 +9,27 @@ namespace EstudoLista11
         static void Main(string[] args)
         {
             Console.Clear();
+
+            Console.WriteLine("=== Escreva 10 valores inteiros ===");
+            var valores = new int[10];
+            int media = 0;
+            Thread.Sleep(1500);
+            for (int i = 0; i < valores.Length; i++)
+            {
+                Console.WriteLine($"Digite o numero {i} da lista");
+                valores[i] = int.Parse(Console.ReadLine());
+                media += valores[i];
+                Console.WriteLine("=================================");
+            }
+
+            var ordenar = valores.OrderByDescending(n => n);
+            var listaOrdenada = ordenar.ToArray();
+
+            Console.Clear();
+
+            Console.WriteLine($"O maior numero dessa lista é o {listaOrdenada[0]}");
+            Console.WriteLine($"O menor numero dessa lista é o {listaOrdenada[listaOrdenada.Length - 1]}");
+            Console.WriteLine($"A média de todos os numeros é {media / 10}");
 
             // === IMPRIMINDO OS MULTIPLOS DE 10 ==
 
