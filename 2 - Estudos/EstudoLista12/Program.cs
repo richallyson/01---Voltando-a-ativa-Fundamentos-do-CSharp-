31e0ec53d3a4f6a2a2b60f280a97608efc5a518b
Richallyson Lima <richallyson@gmail.com>
2022-04-22 19:24:06 -0300

    Adicionando mais uma atividade da lista

--- 2 - Estudos/EstudoLista12/Program.cs
+++ 2 - Estudos/EstudoLista12/Program.cs
@@ -6,7 +6,131 @@ namespace EstudoLista12
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu();
+        }
+
+        public static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("=== CALCULADORA DE DOIS NUMEROS ===");
+            Console.WriteLine("====================");
+            Console.WriteLine("Qual operação deseja realizar?");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Divisão");
+            Console.WriteLine("4 - Multiplicação");
+            var op = int.Parse(Console.ReadLine());
+
+            switch (op)
+            {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Divisao(); break;
+                case 4: Multi(); break;
+            }
+
+        }
+
+        public static void Soma()
+        {
+            Console.Clear();
+            Console.WriteLine("=== SOMA ===");
+            Console.WriteLine("");
+            Console.WriteLine("Digite o primeiro numero");
+            var num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo numero");
+            var num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O resultado da soma é: {num1 + num2}");
+            Console.WriteLine("");
+
+            Console.WriteLine("Deseja voltar para o menu novamente?");
+            Console.WriteLine("Digite S para continar, ou qualquer outra tecla para sair");
+            var escolha = Console.ReadLine().ToLower();
+
+            if (escolha == "s")
+            {
+                Menu();
+            }
+            else
+                Environment.Exit(0);
+        }
+
+        public static void Subtracao()
+        {
+            Console.Clear();
+            Console.WriteLine("=== SUBTRAÇÃO ===");
+            Console.WriteLine("");
+            Console.WriteLine("Digite o primeiro numero");
+            var num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo numero");
+            var num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O resultado da soma é: {num1 - num2}");
+            Console.WriteLine("");
+
+            Console.WriteLine("Deseja voltar para o menu novamente?");
+            Console.WriteLine("Digite S para continar, ou qualquer outra tecla para sair");
+            var escolha = Console.ReadLine().ToLower();
+
+            if (escolha == "s")
+            {
+                Menu();
+            }
+            else
+                Environment.Exit(0);
+        }
+
+        public static void Divisao()
+        {
+            Console.Clear();
+            Console.WriteLine("=== DIVISÃO ===");
+            Console.WriteLine("");
+            Console.WriteLine("Digite o primeiro numero");
+            var num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo numero");
+            var num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O resultado da soma é: {num1 / num2}");
+            Console.WriteLine("");
+
+            Console.WriteLine("Deseja voltar para o menu novamente?");
+            Console.WriteLine("Digite S para continar, ou qualquer outra tecla para sair");
+            var escolha = Console.ReadLine().ToLower();
+
+            if (escolha == "s")
+            {
+                Menu();
+            }
+            else
+                Environment.Exit(0);
+        }
+
+        public static void Multi()
+        {
+            Console.Clear();
+            Console.WriteLine("=== MULTIPLICAÇÃO ===");
+            Console.WriteLine("");
+            Console.WriteLine("Digite o primeiro numero");
+            var num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo numero");
+            var num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O resultado da soma é: {num1 * num2}");
+            Console.WriteLine("");
+
+            Console.WriteLine("Deseja voltar para o menu novamente?");
+            Console.WriteLine("Digite S para continar, ou qualquer outra tecla para sair");
+            var escolha = Console.ReadLine().ToLower();
+
+            if (escolha == "s")
+            {
+                Menu();
+            }
+            else
+                Environment.Exit(0);
+
         }
     }
 }
